ab94c2620c53008fda30682449fdd3bcbfa256c7
James Baker <minorkeys@gmail.com>
2022-08-24 00:12:50 -0400

    Removing commented code

--- Life/Patterns/Pulsar.cs
+++ Life/Patterns/Pulsar.cs
@@ -14,36 +14,6 @@ namespace Life.Patterns
             this.height = 13;
 
             var pattern = new List<Tuple<int, int>>();
-/*            pattern.Add(new Tuple<int, int>(2, 0));
-            pattern.Add(new Tuple<int, int>(3, 0));
-            pattern.Add(new Tuple<int, int>(4, 0));
-            pattern.Add(new Tuple<int, int>(8, 0));
-            pattern.Add(new Tuple<int, int>(9, 0));
-            pattern.Add(new Tuple<int, int>(10, 0));
-
-            pattern.Add(new Tuple<int, int>(0, 2));
-            pattern.Add(new Tuple<int, int>(5, 2));
-            pattern.Add(new Tuple<int, int>(7, 2));
-            pattern.Add(new Tuple<int, int>(12, 2));
-
-            pattern.Add(new Tuple<int, int>(0, 0));
-            pattern.Add(new Tuple<int, int>(5, 5));
-            pattern.Add(new Tuple<int, int>(7, 7));
-            pattern.Add(new Tuple<int, int>(12, 3));
-
-            pattern.Add(new Tuple<int, int>(0, 0));
-            pattern.Add(new Tuple<int, int>(5, 4));
-            pattern.Add(new Tuple<int, int>(7, 4));
-            pattern.Add(new Tuple<int, int>(12, 4));
-
-            pattern.Add(new Tuple<int, int>(2, 5));
-            pattern.Add(new Tuple<int, int>(3, 5));
-            pattern.Add(new Tuple<int, int>(4, 5));
-            pattern.Add(new Tuple<int, int>(8, 5));
-            pattern.Add(new Tuple<int, int>(9, 5));
-            pattern.Add(new Tuple<int, int>(10, 5));*/
-
-
 
             pattern.Add(new Tuple<int, int>(0, 2));
             pattern.Add(new Tuple<int, int>(0, 3));
